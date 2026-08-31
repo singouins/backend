0808024ca30dc7d1ec9e1e9774cc5b0e93387c63
lordslair <5469390+lordslair@users.noreply.github.com>
2026-08-31 10:16:56 +0000

    fix(api): mail.send() error catching

--- api/utils/mail.py
+++ api/utils/mail.py
@@ -19,14 +19,10 @@ def send(adress, subject, body):
     message.attach(message_content)
 
     try:
-        mail = smtplib.SMTP_SSL(env_vars['SMTP_SERVER'], local_hostname=env_vars['SMTP_HOSTNAME'])
-        mail.connect(host=env_vars['SMTP_SERVER'])
-        mail.ehlo()
-        mail.set_debuglevel(0)
-        mail.login(env_vars['SMTP_USER'], env_vars['SMTP_PASS'])
-        mail.sendmail(message["From"], message["To"], message.as_string())
-        mail.close()
-    except smtplib.SMTPException as e:
+        with smtplib.SMTP_SSL(env_vars['SMTP_SERVER'], local_hostname=env_vars['SMTP_HOSTNAME']) as mail:
+            mail.login(env_vars['SMTP_USER'], env_vars['SMTP_PASS'])
+            mail.sendmail(message["From"], message["To"], message.as_string())
+    except (smtplib.SMTPException, OSError) as e:
         logger.error(f'Sending EMail KO [{e}]')
         return False
     else:
